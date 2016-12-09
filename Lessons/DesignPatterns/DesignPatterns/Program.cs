0af00efa5abbf2841269d9a06cfbc3060b54c6ad
Danubius1st <adrian.bucur@hotmail.com>
2016-12-09 09:06:31 +0200

    Project added

---
+++ Lessons/DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        // "Head First Design Patterns" book
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lessons/DesignPatterns/DesignPatterns/StepByStep.cs
@@ -0,0 +1,83 @@
+﻿using DesignPatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class StepByStep
+    {
+    }
+
+    public interface IFlyBehavior
+    {
+        void Fly();
+    }
+
+    public interface IQuackBehavior
+    {
+        void Quack();
+    }
+
+    public abstract class Duck
+    {
+        IFlyBehavior _flyBehavior;
+        IQuackBehavior _quackBehavior;
+
+        public abstract void Display();
+        public void PerformFly()
+        {
+            _flyBehavior.Fly();
+        }
+        public void PerformQuack()
+        {
+            _quackBehavior.Quack();
+        }
+        public void PerformSwim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+
+    public class FlyWithWings : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I’m flying!!");
+            ;
+        }
+    }
+
+    public class FlyNoWay : IFlyBehavior
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I can’t fly");
+            ;
+        }
+    }
+
+    public class Quack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            _quackBehavior = new Quack();
+            _flyBehavior = new FlyWithWings();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I’m a real Mallard duck");
+        }
+    }
+}
