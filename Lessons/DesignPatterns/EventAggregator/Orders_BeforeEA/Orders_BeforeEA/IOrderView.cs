6cd6b5d5e9c61dc8a673cb667af6695b5baa98ea
Danubius1st <adrian.bucur@hotmail.com>
2016-12-15 15:59:53 +0200

    Updates

---
+++ Lessons/DesignPatterns/EventAggregator/Orders_BeforeEA/Orders_BeforeEA/IOrderView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders_BeforeEA
+{
+    public interface IOrderView
+    {
+        void OnOrderSelected();
+        void OnOrderSaved();
+    }
+}
