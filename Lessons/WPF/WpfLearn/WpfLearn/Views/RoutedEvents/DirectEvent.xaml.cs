70b45462519e1775b6d7f080c91f086f9fba41a5
Danubius1st <adrian.bucur@hotmail.com>
2017-01-17 15:23:27 +0200

    Updates

---
+++ Lessons/WPF/WpfLearn/WpfLearn/Views/RoutedEvents/DirectEvent.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLearn.Views.RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for DirectEvent.xaml
+    /// </summary>
+    public partial class DirectEvent : Window
+    {
+        public DirectEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txt1.Text = "Button is Clicked";
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            txt2.Text = "Click event is bubbled to Stack Panel";
+            e.Handled = true; //stop the routed event
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            txt3.Text = "Click event is bubbled to Window";
+        }
+    }
+}
---
+++ Lessons/WPF/WpfLearn/WpfLearn/Views/UserControl1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfLearn.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SetTextProperty =
+         DependencyProperty.Register("SetText", typeof(string), typeof(UserControl1), new
+            PropertyMetadata("", OnSetTextChanged));
+
+        public string SetText
+        {
+            get { return (string)GetValue(SetTextProperty); }
+            set { SetValue(SetTextProperty, value); }
+        }
+
+        private static void OnSetTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UserControl1 userControl1Control = d as UserControl1;
+            if (userControl1Control != null) userControl1Control.OnSetTextChanged(e);
+        }
+        private void OnSetTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            tbTest.Text = e.NewValue.ToString();
+        }
+    }
+}
