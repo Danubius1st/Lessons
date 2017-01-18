3c21a71b4ead8cc97659c3489fc4cbd08471dfb2
Danubius1st <adrian.bucur@hotmail.com>
2017-01-18 16:00:07 +0200

    Updates

--- Lessons/WPF/Resources/ItemTemplateExample/ItemTemplateExample/MainWindow.xaml.cs
+++ Lessons/WPF/Resources/ItemTemplateExample/ItemTemplateExample/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace ItemTemplateExample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// http://www.wpf-tutorial.com/wpf-application/resources/
+    /// http://www.wpf-tutorial.com/listview-control/listview-data-binding-item-template/
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -12,11 +15,25 @@ namespace ItemTemplateExample
             InitializeComponent();
         }
 
-        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        private void BtnClickMe_Click(object sender, RoutedEventArgs e)
         {
             lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
             lbResult.Items.Add(pnlMain.FindResource("strHelloWorld").ToString());
             lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
         }
+
+        private void LvClick_Click(object sender, RoutedEventArgs e)
+        {
+            List<User> items = new List<User>()
+            {
+                new User(){ Name = "John Doe", Age = 42; }
+            };
+        }
+    }
+
+    internal class User
+    {
+    public int Age { get; internal set; }
+    public string Name { get; internal set; }
     }
 }
