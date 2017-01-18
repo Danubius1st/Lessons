8740900619b5bc293f0700d3716a4142979a4200
Danubius1st <adrian.bucur@hotmail.com>
2017-01-18 16:02:21 +0200

    Updates

--- Lessons/WPF/Resources/ItemTemplateExample/ItemTemplateExample/MainWindow.xaml.cs
+++ Lessons/WPF/Resources/ItemTemplateExample/ItemTemplateExample/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
 
 namespace ItemTemplateExample
 {
@@ -10,13 +12,24 @@ namespace ItemTemplateExample
         public MainWindow()
         {
             InitializeComponent();
+
+            List<User> items = new List<User>()
+            {
+                new User(){ Name = "John Doe", Age = 42; }
+            };
         }
 
-        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        private void BtnClickMe_Click(object sender, RoutedEventArgs e)
         {
             lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
             lbResult.Items.Add(pnlMain.FindResource("strHelloWorld").ToString());
             lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
         }
     }
+
+    internal class User
+{
+    public int Age { get; internal set; }
+    public string Name { get; internal set; }
+}
 }
