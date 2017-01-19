8a68968a7fa20a98a25ac07fd907ad5ad8185c39
Danubius1st <adrian.bucur@hotmail.com>
2017-01-19 16:00:32 +0200

    Updates

---
+++ Lessons/WPF/DependencyPropertyExample/DependencyPropertyExample/A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DependencyPropertyExample
+{
+    public class A : DependencyObject
+    {
+        public int Height
+        {
+            get { return (int)GetValue(HeightProperty); }
+            set { SetValue(HeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Height.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeightProperty =
+            DependencyProperty.Register("Height", typeof(int), typeof(A), new PropertyMetadata(0));
+
+        public B BObject { get; set; }
+    }
+}
---
+++ Lessons/WPF/DependencyPropertyExample/DependencyPropertyExample/B.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace DependencyPropertyExample
+{
+    public class B : DependencyObject
+    {
+        public static readonly DependencyProperty HeightProperty;
+        public B()
+        {
+            HeightProperty = A.HeightProperty.AddOwner(typeof(B), new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.Inherits))
+        }
+    }
+}
