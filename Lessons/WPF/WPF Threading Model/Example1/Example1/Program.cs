328b0c74127db2a49481cd5df40539d8394eb9ac
Adrian Bucur <adrian.bucur@hotmail.com>
2019-09-16 21:54:38 +0300

    Added WPF Threading Model\Example1

---
+++ Lessons/WPF/WPF Threading Model/Example1/Example1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Example1
+{
+    class Program
+    {
+        // https://owlcation.com/stem/C-ThreadPool-and-its-Task-Queue-Example
+
+        private static void TaskCallBack(Object ThreadNumber)
+        {
+            string ThreadName = "Thread " + ThreadNumber.ToString();
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine(ThreadName + ": " + i.ToString());
+            }
+            Console.WriteLine(ThreadName + " Finished...");
+        }
+
+        static void Main(string[] args)
+        {
+            for (int task = 1; task < 11; task++)
+            {
+                _ = ThreadPool.QueueUserWorkItem(new WaitCallback(TaskCallBack), task);
+            }
+            //Thread.Sleep(10000);
+            _ = Console.ReadKey();
+        }
+    }
+}
